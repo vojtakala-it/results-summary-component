c726514b36ce8213ae2aa9bc8c115786e7a0a55d
n3ey <vojta.kala.it@gmail.com>
2023-11-11 18:37:45 +0100

    feat(deps): RSC-4 remove unused dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -43,21 +43,7 @@ const config = {
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: "asset"
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: 'assets/images[hash]-[name].[ext]',
-                            limit: 8192,
-                            fallback: 'file-loader'
-                        }
-                    }
-                ]
             }
-
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
         ]
